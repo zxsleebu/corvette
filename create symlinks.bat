57923072c291752e358cc0f9702a9bf1837b605e
sleebu <46263989+zxsleebu@users.noreply.github.com>
2022-07-15 02:47:28 +0200

    Create create symlinks.bat

---
+++ create symlinks.bat
@@ -0,0 +1,3 @@
+mklink /H "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\primordial\scripts\corvette.lua" 			"./corvette.lua"
+mklink /H "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\primordial\scripts\corvette-build.lua" 		"./corvette-build.lua"
+mklink /J "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\primordial\scripts\include\corvette_lib" 	"./corvette_lib"
